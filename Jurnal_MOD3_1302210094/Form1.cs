dceba61e14491ce218fac83c8fd6837d31b49f60
PRAKTIKAN <PRAKTIKAN@0605-PC-21>
2023-03-06 07:01:54 +0700

    Add project files.

---
+++ Jurnal_MOD3_1302210094/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jurnal_MOD3_1302210094
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
